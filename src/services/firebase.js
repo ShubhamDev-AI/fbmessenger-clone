747e135ae2b095b135f50216bd330c4eda60044a
tquangdo <trangia61@gmail.com>
2020-08-29 01:38:27 +0900

    20200829 1:38

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,9 +2,9 @@ import firebase from "firebase/app"
 import "firebase/auth"
 import "firebase/database"
 const config = {
-  apiKey: "AIzaSyCVI6TyvqJLD9zns7t14iK3zUSE48tFFRw",
-  authDomain: "fbmessengerclone-aaa62.firebaseapp.com",
-  databaseURL: "https://fbmessengerclone-aaa62.firebaseio.com",
+  apiKey: "xxxxxxxxxxxx",
+  authDomain: "xxxxxxxxxxxx",
+  databaseURL: "xxxxxxxxxxxx",
   // projectId: "app-name",
   // storageBucket: "app-name.appspot.com",
   // messagingSenderId: "xxxxxxxxxxxx",
